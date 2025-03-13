17f0a341a190fe39a59d28c06e611988eb8ef1d6
judit Bardon <juditbardonromero@gmail.com>
2025-03-13 18:07:04 +0000

    changed borders and bg-color in light theme

--- Judit-porfolio/src/components/Toolkit.tsx
+++ Judit-porfolio/src/components/Toolkit.tsx
@@ -20,7 +20,7 @@ function Toolkit({img, name}: ToolkitProps){
                     </div>
                 }  
                 {theme === 'light' && 
-                    <div className="border-2 border-blue-300 bg-blue-100 h-26 w-32 flex flex-col items-center center p-2 rounded-3xl gap-2">
+                    <div className="h-26 w-32 flex flex-col items-center center p-2 rounded-3xl gap-2">
                         <img src={img} alt="" className="h-1/2"/>
                         <p className="h-1/2">{name}</p>
                     </div>
