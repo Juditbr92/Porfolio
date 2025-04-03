947c2e08c2b1cbe28af9aeccdfbe322e246816ff
judit Bardon <juditbardonromero@gmail.com>
2025-04-03 20:27:54 +0100

    responsive About Me page

--- Judit-porfolio/src/components/About.tsx
+++ Judit-porfolio/src/components/About.tsx
@@ -12,14 +12,12 @@ function About({theme, title, description}: AboutProps){
     const textColor = isDark ? 'text-white' : 'text-black'
     
     return(
-        <div className="flex align-middle">
-            <div className="flex flex-col justify-center pt-8 ml-4 px-12 h-[80vh] w-1/2">
-                <h1>{title}</h1>
-                <p className={`text-md mt-4 leading-relaxed  ${textColor}`}>{description}</p>
-            </div>
-            
-            <div className="flex justify-center items-center h-[80vh] w-1/2">
-                <img className= "h-[80%] rounded-3xl" src="/foto.jpg" alt="Foto" />
+        <div className="h-full flex flex-col align-middle">
+            <h1 className="mt-20 mx-auto flex">{title}</h1>
+            <div className="h-[60vh] w-[99vw] flex justify-between 2xl:ml-12 px-12 gap-4 items-center xl:gap-12">
+                <p className={`text-base xl:text-lg 2xl:text-xl mt-4 leading-relaxed mb-4 md:max-w-[500px] xl:max-w-[800px] 2xl:min-w-[1/2] ${textColor}`}>{description}</p>
+                <img className="hidden md:flex md:h-[50%] md:w-[50%] lg:mb-8 xl:min-h-[400px] xl:min-w-[400px] xl:max-w-[500px] rounded-3xl object-cover aspect-square"  
+                    src="/foto.jpg" alt="Foto" />
             </div>
             
         </div>
