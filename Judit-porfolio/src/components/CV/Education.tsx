1e48a3b7c8632d71433926ac9126d122c9c890e4
judit Bardon <juditbardonromero@gmail.com>
2025-04-16 18:25:34 +0100

    grammar correction in Cv

--- Judit-porfolio/src/components/CV/Education.tsx
+++ Judit-porfolio/src/components/CV/Education.tsx
@@ -1,5 +1,5 @@
 export const education = [
-    { year: "2025", titulo: "Curso React en Codenotch", title: "REACT curse - Codenotch" },
+    { year: "2025", titulo: "Curso React en Codenotch", title: "REACT course - Codenotch" },
     { year: "2023 2024", titulo: "Bootcamp Desarrollo Web Full Stack en Codenotch", title: "Bootcamp Web Development Full Stack - Codenotch" },
     { year: "2022", titulo: "Tutorial JavaScript en Codecademy", title: "JavaScript tutorial - Codecademy" },
     { year: "2022", titulo: "Tutorial Ruby en Codecademy", title: "Ruby tutorial - Codecademy" },
